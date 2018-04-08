6e887aae8e9dddaf37edd8f4bf52f84971b993ae
frontich <bostjan267@gmail.com>
2018-04-08 22:06:44 +0200

    move json data to service, will be replaced with api call

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { NavBarComponent } from './nav/navbar.component'; 
+import { NavBarComponent } from './nav/navbar.component';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
     declarations: [
@@ -14,6 +16,7 @@ import { EventThumbnailComponent } from './events/event-thumbnail.component';
         EventsListComponent,
         EventThumbnailComponent
     ],
+    providers: [EventService],
     imports: [BrowserModule],
     bootstrap: [EventsAppComponent]
 })
