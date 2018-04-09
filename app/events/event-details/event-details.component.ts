24ab554f6288927c943a68a7c20fa250cfc5f791
frontich <bostjan267@gmail.com>
2018-04-09 18:55:57 +0200

    use activated route service for routing around

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { EventService } from '../shared/event.service';
 
@@ -6,8 +7,8 @@ import { EventService } from '../shared/event.service';
     selector: 'event-details',
     templateUrl: '/app/events/event-details/event-details.component.html',
     styles: [`
-        .container: { padding-left:20px; padding-right:20px; }
-        .event-image: { height:100px;}
+        .container { padding-left:20px; padding-right:20px; }
+        .event-image { height:100px;}
     `]
 
 
@@ -15,11 +16,16 @@ import { EventService } from '../shared/event.service';
 export class EventDetailsComponent implements OnInit {
     event: any;
 
-    constructor(private eventService: EventService) {
+    constructor(
+        private eventService: EventService,
+        private route: ActivatedRoute
+    ) {
 
     }
 
     ngOnInit(): void {
-       this.event =  this.eventService.getEvent(1); // hardcoded for now
+        this.event = this.eventService.getEvent(
+            +this.route.snapshot.params['id']
+        ); // hardcoded for now
     }
 }
