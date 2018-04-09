83db7a4f4612a35e20f33797dc29613f5ff8f654
frontich <bostjan267@gmail.com>
2018-04-09 21:54:29 +0200

    add route to sub module

--- app/routes.ts
+++ app/routes.ts
@@ -22,5 +22,6 @@ export const appRoutes: Routes = [
         canActivate: [EventRouteActivator]
     },
     { path: '404', component: Error404Component },
-    { path: '', redirectTo: '/events', pathMatch: 'full' }
+    { path: '', redirectTo: '/events', pathMatch: 'full' },
+    { path:'user', loadChildren:'/app/user/user.module#UserModule'}
 ]
